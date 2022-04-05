a59c45f761eec4307e739dcb57ac1a6547c52bfa
Suyeon04 <s2026@e-mirim.hs.kr>
2022-04-06 00:00:50 +0900

    ..

--- src/public/js/game.js
+++ src/public/js/game.js
@@ -1,12 +1,31 @@
 const now = document.querySelector('#now');
 const next = document.querySelector('#next');
-
+const nowinput = document.querySelector('#nowinput');
+const nextinput = document.querySelector('#nextinput');
 
 let nowkeyword = "My name is Suyeon";
 let nextkeyword = "My name is hyuna";
+let alp = "";
+let keywords = "";
+let alpLength = 0;
 
 function change(){
     now.innerText = nowkeyword;
     next.innerText = nextkeyword;
 }
+nowinput.oninput = function(){
+    document.addEventListener("keydown",isRight, false);
+    let inputvalue = nowinput.value;
+    alp = (inputvalue||"").split("");
+    keywords = (nowkeyword||"").split("");
+    if(alp[alp.length-1]!=keywords[alp.length-1]){
+        
+    }
+
+}
+
+function isRight(){
+
+}
+
 change();
