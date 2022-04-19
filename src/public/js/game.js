efe0c7c2038db0388aa7cb470cb015908c7319bf
yuhyna108 <s2012@e-mirim.hs.kr>
2022-04-19 22:27:54 +0900

    0419
    내가 입력 한 적 없는 내용이 생성됨..

--- src/public/js/game.js
+++ src/public/js/game.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let cnt=0;
 $($("#btn").on("click", function(){
     ++cnt;
@@ -15,38 +14,3 @@ $($("#btn").on("click", function(){
         })
     }
 }))
-=======
-const now = document.querySelector('#now');
-const next = document.querySelector('#next');
-const nowinput = document.querySelector('#nowinput');
-const nextinput = document.querySelector('#nextinput');
-
-let nowkeyword = "My name is Suyeon";
-let nextkeyword = "My name is hyuna";
-let alp = "";
-let keywords = "";
-let alpLength = 0;
-
-function change(){
-    now.innerText = nowkeyword;
-    next.innerText = nextkeyword;
-}
-nowinput.oninput = function(){
-    document.addEventListener("keydown",isRight, false);
-    let inputvalue = nowinput.value;
-    alp = (inputvalue||"").split("");
-    keywords = (nowkeyword||"").split("");
-    if(alp[alp.length-1]!=keywords[alp.length-1]){
-        now.style.color = "#228B22";
-    }else{
-
-    }
-
-}
-
-function isRight(){
-
-}
-
-change();
->>>>>>> 19eacc18ba74f6009a075a98e7da67e236d1cd9a
