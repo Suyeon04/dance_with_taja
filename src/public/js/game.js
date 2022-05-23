6d911068af05beefc871c2a13c111dd9c19c8374
yuhyna108 <s2012@e-mirim.hs.kr>
2022-05-23 21:56:15 +0900

    타자기능추가
    글자 하나하나에 대해서 타자가 틀리면 빨간색으로 바뀜, 타자가 맞으면 초록색으로 바뀜 => 한번 친 다음 타자를 고치면 색깔이 안 바뀌는 오류가 있음
    타자를 다 치고 나면 div가 사라졌다가 다시 나타남 , 올바르게 타자를 치면 캐릭터가 움직임(지금은 리정만)

--- src/public/js/game.js
+++ src/public/js/game.js
@@ -1,6 +1,67 @@
+const str = "Type this as fast as possible."
 
-let wordDisplay = document.querySelector('#word-display'); // 타자미리보기
-let wordinput = document.querySelector('#word-input'); // 타자치는 곳
+let text = document.querySelector('.word-display'); // 타자미리보기
+let input = document.querySelector('.word-input'); // 타자치는 곳
+const charEls = []
+
+function populateText(str){
+    str.split("").map(letter => {
+        const span = document.createElement("span")
+        span.innerText = letter
+        text.appendChild(span)
+
+        charEls.push(span)
+    })
+}
+populateText(str)
+
+function resetCharEls(){
+    charEls.map(charEl => {
+        charEl.classList.remove("correct")
+        charEl.classList.remove("wrong")
+    })
+}
+input.addEventListener("keyup", () => {
+    const val = input.value
+    let errorCount = 0
+    let start=false;
+    val.split("").map((letter, i) => {
+        if(letter === str[i]){
+            charEls[i].classList.add("correct")
+        }else{
+            charEls[i].classList.add("wrong")
+            errorCount++
+        }
+    })
+
+    if(val.length == str.length){
+        $(function(){
+            $("#out2").animate({opacity:0, top:'-40px'},1500);
+            start=true
+        })
+        $(function(){
+            if(errorCount === 0){
+                console.log("Well Done!")
+                for(let i=2; i<10; i++){
+                    (x => {
+                        setTimeout(() => {
+                        console.log(x)
+                        let str="/css/r"+x+".jpg"
+                        char_r.src=str
+                        },150*x)
+                    })(i)
+                }
+            }
+        })
+        setTimeout(()=>{
+            $(function(){
+                $("#out2").remove();
+                $("<div class=box id=out2> <input type=text class=word-input placeholder=타자치는곳2 autofocus> </div>").appendTo("#taja");
+            })
+        },1600); 
+    }
+})
+
 
 let cnt=0
 const char_r = document.getElementById("mychar"); // 리정 이미지
@@ -29,32 +90,3 @@ $($("#btn").on("click", function(){
         })
     }
 }))
-
-let nowkeyword = "My name is Suyeon";
-let nextkeyword = "My name is hyuna";
-let alp = "";
-let keywords = "";
-let alpLength = 0;
-
-function change(){
-    now.innerText = nowkeyword;
-    next.innerText = nextkeyword;
-}//걍 input으로 하는데 밑줄을 그어버릴까?
-nowinput.oninput = function(){
-    document.addEventListener("keydown",isRight, false);
-    let inputvalue = nowinput.value;
-    alp = (inputvalue||"").split("");
-    keywords = (nowkeyword||"").split("");
-    if(alp[alp.length-1]!=keywords[alp.length-1]){
-        now.style.color = "#228B22";
-    }else{
-
-    }
-
-}
-
-function isRight(){
-
-}
-
-change();
