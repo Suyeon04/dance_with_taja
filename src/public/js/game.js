e055fd8a1600eb95fe886fa877149cfeb02425b1
yuhyna108 <s2012@e-mirim.hs.kr>
2022-06-15 09:11:47 +0900

    game.js 수정
    input 나타나는 애니메이션 수정함!
    전에는 새로 생성했다면 지금은 처음에 있던 input을 가지고 마치 사라지고 생성되는 것처럼 애니메이션을 만듦!

--- src/public/js/game.js
+++ src/public/js/game.js
@@ -1,72 +1,59 @@
 const x = 
 `<!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Document</title>
-<style>
-table {
-width: 50%;
-border-top: 1px solid #444444;
 border-collapse: collapse; }
-th, td {
 border-bottom: 1px solid #444444;
-padding: 10px; }
 input:focus { background:rgb(250, 217, 157); }
-</style>
-</head>
-<body>
 <form method="post" action="insert.php" enctype="multipart/form-data">
 <table  cellspacing=0 cellpadding=0>
 <tr><td >제목: <input type="text" name="title" size="10">
-<tr><td >저자: <input type="text" name="author" size="10">
-<tr><td >가격: <input type="text" name="price" size="10">
-<tr><td >표지: <input type="file" name="image" size="10">
-<tr><td >상세정보: <input type="text" name="description" size="10">
-<tr><td style="border-bottom:0" colspan="2" align="center">
-<button type="submit">입력완료</button></td></tr>
-</table>
-</form> 
-</body>
-</html>`;
+<button type="submit">입력완료</button></td></tr>`;
+
 let str = x.split('\n');
 let input_text = '';
 
 const char_r = document.getElementById("mychar"); // 리정 이미지
 const char_n = document.getElementById("youchar"); // 노제 이미지
 let text = document.querySelector('.word-display'); // 타자미리보기
-let input = document.querySelector('.word-input'); // 타자치는 곳
+let input = document.querySelector('.word-input'); //타자치는곳
 let charEls = [];
 let order = 0;
 
 changeWord();
+
+function changeWord(){
+    order++;
+    populateText(str[order]);
+    
+}
+
 function populateText(str){
     charEls=[];
-    console.log(charEls)
+    
     str.split("").map(letter => {
         const span = document.createElement("span")
         span.innerText = letter
-        span.style.color = '#808080';
+        // span.style.color = '#808080';
         text.appendChild(span)
         charEls.push(span)
     })
 }
 
+// 어디에 필요한 함수일까..? 없어도 잘 돌아가긴 함
+// function resetCharEls(){
+//     charEls.map(charEl => {
+//         charEl.classList.remove("correct")
+//         charEl.classList.remove("wrong")
+//     })
+// }
 
-function resetCharEls(){
-    charEls.map(charEl => {
-        charEl.classList.remove("correct")
-        charEl.classList.remove("wrong")
-    })
-}
-function changeWord(){
-    order++;
-    populateText(str[order]);
+function removeCorrectCharacter() { // 친 글자 사라지는 함수
+    document.querySelectorAll('.correct').forEach(item => item.remove());
 }
 
-input.addEventListener("keyup", () => {
+input.addEventListener("keyup", (event) => {
     const val = input.value
     let errorCount = 0;
     let start=false;
@@ -81,41 +68,46 @@ input.addEventListener("keyup", () => {
     })
     if(val.length == str[order].length){
         $(function(){
-            $("#out2").animate({opacity:0, top:'-40px'},1500);
-            start=true
-        })
-        $(function(){
-            console.log(charEls)
+            console.log(errorCount)
             if(errorCount === 0){
+
+                $(function(){
+                    start=true;
+                    $("#out2").animate({opacity:0, top:'-35px'},1500); // 타자를 친 후 애니메이션
+                    /*!! input이 사라지는게 아니라 하나의 input을 가지고 사라지는 효과만 내줌 !! */
+                    removeCorrectCharacter(); // 친 글자 사라지기
+                })
                 console.log("Well Done!")
+
+                // 리정 애니메이션
                 for(let i=2; i<10; i++){
                     (x => {
                         setTimeout(() => {
-                        console.log(x)
                         let str="/img/ygx"+x+".png"
                         char_r.src=str
                         },150*x)
                     })(i)
                 }
 
-                for(let i=2;i<12;i++){
-                    (y => {
-                        setTimeout(() => {
-                        console.log(y)
-                        let str="/img/wavy"+y+".png"
-                        char_n.src=str
-                        },150*y)
-                    })(i)
-                }
+                // 노제 애니메이션
+                // for(let i=2;i<12;i++){
+                //     (y => {
+                //         setTimeout(() => {
+                //         let str="/img/wavy"+y+".png"
+                //         char_n.src=str
+                //         },150*y)
+                //     })(i)
+                // }
+
+                $(function(){
+                    $("#out2").animate({opacity:100, top:'40px'}); // input 나타나는 애니메이션
+                    start=true;
+                    input.value=null; // input 나타난 후 썼던 글자 지워주기
+                })
+
+                changeWord() // 다음 타자로 넘어가기
             }
         })
-        setTimeout(()=>{
-            $(function(){
-                $("#out2").remove();
-                $("<div class=box id=out2> <input type=text class=word-input placeholder=타자치는곳2 autofocus> </div>").appendTo("#taja");
-                changeWord();
-            })
-        },1600); 
-        
     }
 })
+
